43b45ed6644dfb89ebc11717f07aadda786497a8
Alonso Ortega <alonsojortega@gmail.com>
2019-02-07 21:00:31 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -13,7 +13,7 @@ def run_guessing_game
       puts "Goodbye!"
       break
     else
-      puts "The computer guessed #{random_number}."
+      puts "The computer guessed #{rand_num."
     end
   end
 end
